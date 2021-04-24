d56ae14cebb34bf9bc6eb0ae83fbe6780acc69bb
priyanksaluja <30635083+priyanksaluja@users.noreply.github.com>
2021-04-24 20:46:29 +0530

    Update settings.py

--- src/myfirstvirtualassistant/settings.py
+++ src/myfirstvirtualassistant/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(f$5(v4-3&n*^&i5yrc%w_jp(nse=sqe1yy&b+@-+==t=&lhm9'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
